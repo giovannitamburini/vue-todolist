bfcc1f0ac50c117eeec75130894e397c748316a8
giovannitamburini <giovatambu@gmail.com>
2023-03-15 16:27:47 +0100

    added milestone 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            todos: [
+                {
+                    text: 'fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'andare dal ferramenta',
+                    done: false
+                },
+                {
+                    text: 'andare in farmacia',
+                    done: false
+                }
+
+            ]
         }
+
     }
 }).mount('#app')
